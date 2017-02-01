7f94c9dea5f759b941f60c7b3eeea3c50411e683
Ben Costrell <bencostrell@gmail.com>
2017-02-01 13:44:45 -0500

    word generation

---
+++ Assets/Scripts/WordController.cs
@@ -0,0 +1,27 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class WordController : MonoBehaviour {
+
+	public string text;
+	public string partOfSpeech;
+	public float characterLength;
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+
+	public void SetText(string textToSet, string pofSpeech){
+		text = textToSet;
+		partOfSpeech = pofSpeech;
+		GetComponent<TextMesh> ().text = textToSet;
+		BoxCollider2D boxCol = GetComponent<BoxCollider2D>();
+		boxCol.size = new Vector2 (characterLength * text.Length, boxCol.size.y);
+	}
+}
---
+++ Assets/Scripts/WordGenerator.cs
@@ -0,0 +1,68 @@
+﻿using UnityEngine;
+using System.Collections;
+using System.Collections.Generic;
+
+public class WordGenerator : MonoBehaviour {
+
+	public List<string> verbs;
+	public List<string> adjectives;
+	public List<string> nouns;
+
+	public GameObject wordPrefab;
+	public float minAcceptableDistance;
+	public int numEachPos;
+
+	private List<GameObject> wordList;
+
+	// Use this for initialization
+	void Start () {
+		GenerateWords ();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+
+	void GenerateWords(){
+		wordList = new List<GameObject> ();
+		for (int i = 0; i < numEachPos; i++) {
+			GenerateWord (verbs);
+			GenerateWord (adjectives);
+			GenerateWord (nouns);
+		}
+	}
+
+	public void GenerateWord(List<string> poSpeechArray){
+		int wordIndex = Random.Range (1, poSpeechArray.Count);
+		string wordText = poSpeechArray [wordIndex];
+		Vector3 location = GenerateRandomLocation ();
+		GameObject word = Instantiate (wordPrefab, location, Quaternion.identity) as GameObject;
+		word.GetComponent<WordController> ().SetText (wordText, poSpeechArray [0]);
+		wordList.Add (word);
+	}
+
+	Vector3 GenerateRandomLocation(){
+		Vector3 location = new Vector3 (Random.Range (-3f, 3f), Random.Range (-4.5f, 4.5f), 0);
+		bool accept = ValidateLocation (location);
+		while (!accept) {
+			location = new Vector3 (Random.Range (-3f, 3f), Random.Range (-4.5f, 4.5f), 0);
+			accept = ValidateLocation (location);
+		}
+		return location;
+	}
+
+	bool ValidateLocation(Vector3 location){
+		bool accept = true;
+		if (wordList.Count > 0) {
+			foreach (GameObject word in wordList) {
+				if (Vector3.Distance (location, word.transform.position) < minAcceptableDistance) {
+					accept = false;
+					break;
+				} 
+			}
+		}
+		return accept;
+	}
+
+}
