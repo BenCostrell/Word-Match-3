0095cb4e63edf2259fb6765f9c2cb64aa48b1c43
Ben Costrell <bencostrell@gmail.com>
2017-02-01 15:15:40 -0500

    initial shape structure

---
+++ Assets/Scripts/ShapeController.cs
@@ -0,0 +1,27 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class ShapeController : MonoBehaviour {
+
+	public string shape;
+	public Color color;
+	public ShapeManager shapeManager;
+
+	// Use this for initialization
+	void Awake () {
+		shapeManager = GameObject.FindGameObjectWithTag ("ShapeManager").GetComponent<ShapeManager> ();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+
+	public void InitializeProperties(string newShape, Color newColor){
+		shape = newShape;
+		color = newColor;
+		SpriteRenderer sr = GetComponent<SpriteRenderer> ();
+		sr.sprite = shapeManager.GetSpriteByName (newShape);
+		sr.color = newColor;
+	}
+}
---
+++ Assets/Scripts/ShapeManager.cs
@@ -0,0 +1,59 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class ShapeManager : MonoBehaviour {
+
+	public Sprite circleSprite;
+	public Sprite squareSprite;
+	public Sprite triangleSprite;
+
+	public GameObject shapePrefab;
+
+	public Vector3 nextSpawnLocation;
+
+	public GameObject[,] shapeGrid;
+
+	// Use this for initialization
+	void Start () {
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+
+	void InitializeShapeGrid(){
+		shapeGrid = new GameObject[5, 5];
+	}
+
+	public Sprite GetSpriteByName(string spriteName){
+		Sprite spriteToReturn;
+		if (spriteName == "circle") {
+			spriteToReturn = circleSprite;
+		}
+		else if (spriteName == "square") {
+			spriteToReturn = squareSprite;
+		}
+		else {
+			spriteToReturn = triangleSprite;
+		}
+		return spriteToReturn;
+	}
+
+	public Color GetColorByName(string colorString){
+		Color colorToReturn;
+		if (colorString == "red") {
+			colorToReturn = Color.red;
+		} else if (colorString == "green") {
+			colorToReturn = Color.green;
+		} else {
+			colorToReturn = Color.blue;
+		}
+		return colorToReturn;
+	}
+
+	public void CreateShape(string newShape, Color newColor){
+		GameObject newShapeObject = Instantiate (shapePrefab, nextSpawnLocation, Quaternion.identity) as GameObject;
+		newShapeObject.GetComponent<ShapeController> ().InitializeProperties (newShape, newColor);
+	}
+}
